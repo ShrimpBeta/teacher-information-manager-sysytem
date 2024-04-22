54c28f28af93ea4b352729ed5cf83e232b1f9708
Shrimp <81841997+ShrimpBeta@users.noreply.github.com>
2024-04-22 17:28:56 +0800

    update

--- miniapp/src/pages/editclassschedule/index.config.ts
+++ miniapp/src/pages/editclassschedule/index.config.ts
@@ -1,3 +1,3 @@
 export default definePageConfig({
-  navigationBarTitleText: '编辑课表',
+  navigationBarTitleText: '查看课表',
 })
--- miniapp/src/pages/editclassschedule/index.tsx
+++ miniapp/src/pages/editclassschedule/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import './index.scss'
 import { getCurrentInstance } from '@tarojs/runtime'
-import { useLazyQuery, useMutation } from '@apollo/client'
-import { createCourseMutation, deleteCourseMutation, updateAcademicTerm, updateCourseMutation } from '@/graphql/mutation/classschedule.mutation.graphql'
+import { useLazyQuery } from '@apollo/client'
 import { academicTermQuery } from '@/graphql/query/classschedule.query.graphql'
 import { ClassSchedule, Course } from '@/models/models/classSchedule.model'
+import { Button } from '@nutui/nutui-react-taro'
+import { ArrowLeft, ArrowRight } from '@nutui/icons-react-taro'
 
 class CourseCell {
   couresId: string | null = null;
@@ -25,10 +26,6 @@ function Index() {
   const daysOfWeek = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
   const periods = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二', '十三'];
 
-  const [updateClassSchedule] = useMutation(updateAcademicTerm);
-  const [crateCourse] = useMutation(createCourseMutation);
-  const [updateCourse] = useMutation(updateCourseMutation);
-  const [deleteCourse] = useMutation(deleteCourseMutation);
   const [classScheduleQuery] = useLazyQuery(academicTermQuery)
 
   const [classSchedule, setClassSchedule] = useState<ClassSchedule | null>(null);
@@ -37,6 +34,18 @@ function Index() {
   const [classScheduleMatrix, setClassScheduleMatrix] = useState<CourseCell[][]>(Array(7).fill(null).map(() => Array(periods.length).fill(null)));
 
   const $instance = getCurrentInstance()
+
+  const getWeekNumber = (startDate: Date): number => {
+    let now = new Date();
+    let start = new Date(startDate);
+    let diffInTime = now.getTime() - start.getTime();
+    let diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));
+    let startDay = start.getDay();
+    let nowDay = now.getDay();
+    let weekNumber = Math.floor(diffInDays / 7);
+    if (startDay >= nowDay) weekNumber++;
+    return weekNumber;
+  }
 
   useEffect(() => {
     let id = $instance.router?.params.id
@@ -50,6 +59,9 @@ function Index() {
         console.log(classSchedule)
         setClassSchedule(classSchedule);
         updateClassScheduleMatrix(classSchedule.courses);
+        const week = getWeekNumber(new Date(classSchedule.startDate))
+        setNowWeek(week);
+        setCurrentWeek(week);
       })
     }
   }, []);
@@ -87,47 +99,56 @@ function Index() {
     <View className="container">
       {classSchedule &&
         <>
-          <View></View>
-          <View>
-            <View className='table'>
-              <View className='thead'>
-                <View className='tr'>
-                  <View className='th'>时间</View>
-                  {daysOfWeek.map((dayOfWeek, index) => {
-                    return <View key={index} className='th'>{dayOfWeek}</View>
-                  })}
-                </View>
-              </View>
-              <View className='tbody'>
+          <View style={{ width: '100%', height: '120rpx', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Button fill="none" style={{ margin: '8' }} disabled={currentWeek == 1} onClick={() => { if (currentWeek > 1) { setCurrentWeek(currentWeek - 1) } }}>
+              <ArrowLeft />
+            </Button>
+            <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <View>{classSchedule.termName}</View>
+              <View>第 {currentWeek} 周</View>
+              {nowWeek !== currentWeek && <View>当前为第 {nowWeek} 周</View>}
+            </View>
+            <Button fill="none" style={{ margin: '8' }} disabled={currentWeek == classSchedule.weekCount} onClick={() => { if (currentWeek < classSchedule.weekCount) { setCurrentWeek(currentWeek + 1) } }}>
+              <ArrowRight />
+            </Button>
+          </View>
+
+          <View style={{ height: '2500rpx', overflow: 'auto' }}>
+            <View style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+              <View style={{ display: 'flex', flexDirection: 'column', flex: '1', borderRight: '1px solid #f0f5fe', borderLeft: '1px solid #f0f5fe' }}>
+                <View style={{ flex: '0.5', borderBottom: '1px solid #f0f5fe', borderTop: '1px solid #f0f5fe', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>时间</View>
                 {periods.map((period, periodIndex) => {
-                  return (
-                    <View key={periodIndex} className='tr'>
-                      <View className='td'>{period}</View>
-                      {daysOfWeek.map((dayOfWeek, dayOfWeekIndex) => {
-                        let courseCell = classScheduleMatrix[dayOfWeekIndex][periodIndex];
-                        if (courseCell === null || courseCell === undefined) {
-                          return <View key={dayOfWeekIndex} className='td'></View>
-                        }
-                        if (courseCell.start === periodIndex + 1 && courseCell.courseWeeks?.includes(currentWeek)) {
-                          return (
-                            <View key={dayOfWeekIndex} className='td' style={{ flex: courseCell.end! - courseCell.start! + 1, backgroundColor: courseCell.color! }}>
-                              {courseCell &&
-                                <View>
-                                  <View>{courseCell.courseName}</View>
-                                  <View>{courseCell.courseLocation}</View>
-                                  <View>{courseCell.teacherNames}</View>
-                                </View>
-                              }
-                            </View>
-                          )
-                        } else if (!courseCell.courseWeeks?.includes(currentWeek)) {
-                          return <View key={dayOfWeekIndex} className='td'></View>
-                        }
-                      })}
-                    </View>
-                  )
+                  return <View key={periodIndex} style={{ flex: '1', borderBottom: '1px solid #f0f5fe', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{period}</View>
                 })}
               </View>
+              {daysOfWeek.map((dayOfWeek, dayOfWeekIndex) => {
+                return (
+                  <View key={dayOfWeekIndex} style={{ display: 'flex', flexDirection: 'column', flex: '1', borderRight: '1px solid #f0f5fe' }}>
+                    <View style={{ flex: '0.5', borderBottom: '1px solid #f0f5fe', borderTop: '1px solid #f0f5fe', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>{dayOfWeek}</View>
+                    {periods.map((period, periodIndex) => {
+                      let courseCell = classScheduleMatrix[dayOfWeekIndex][periodIndex];
+                      if (courseCell === null || courseCell === undefined) {
+                        return <View key={periodIndex} style={{ flex: '1', borderBottom: '1px solid #f0f5fe', display: 'flex', justifyContent: 'center', alignItems: 'center' }}></View>
+                      }
+                      if (courseCell.start === periodIndex + 1 && courseCell.courseWeeks?.includes(currentWeek)) {
+                        return (
+                          <View key={periodIndex} style={{ flex: courseCell.end! - courseCell.start! + 1, backgroundColor: courseCell.color!, borderBottom: '1px solid #f0f5fe', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '20rpx', color: 'white' }}>
+                            {courseCell &&
+                              <View>
+                                <View style={{ wordBreak: 'break-all' }}>{courseCell.courseName}</View>
+                                <View style={{ wordBreak: 'break-all' }}>{courseCell.courseLocation}</View>
+                                <View style={{ wordBreak: 'break-all' }}>{courseCell.teacherNames}</View>
+                              </View>
+                            }
+                          </View>
+                        )
+                      } else if (!courseCell.courseWeeks?.includes(currentWeek)) {
+                        return <View key={periodIndex} style={{ flex: '1', borderBottom: '1px solid #f0f5fe', display: 'flex', justifyContent: 'center', alignItems: 'center' }}></View>
+                      }
+                    })}
+                  </View>
+                )
+              })}
             </View>
           </View>
         </>
