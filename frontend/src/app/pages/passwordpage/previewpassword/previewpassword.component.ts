2f23a98cfc60f3f7472ca9cbba048d06296590fc
Shrimp <81841997+ShrimpBeta@users.noreply.github.com>
2024-05-10 14:18:00 +0800

    update upload

--- frontend/src/app/pages/passwordpage/previewpassword/previewpassword.component.ts
+++ frontend/src/app/pages/passwordpage/previewpassword/previewpassword.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-previewpassword',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatProgressBarModule, MatCardModule],
   templateUrl: './previewpassword.component.html',
   styleUrl: './previewpassword.component.scss'
 })
 export class PreviewpasswordComponent {
+  constructor(
+    private snackBar: MatSnackBar
+  ) { }
+
+  progress = 0;
+  file: File | null = null;
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      if (this.fileTypeCheck(input.files[0])) {
+        this.file = input.files[0];
+      } else {
+        this.snackBar.open('文件格式错误', '关闭', { duration: 2000 })
+      }
+    }
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      if (this.fileTypeCheck(files[0])) {
+        this.file = files[0];
+      } else {
+        this.snackBar.open('文件格式错误', '关闭', { duration: 2000 })
+      }
+    }
+  }
+
+  fileTypeCheck(file: File) {
+    return (file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'application/pdf' || file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+  }
+
+  onUpload() {
+    if (this.file) {
+
+    }
+  }
 
 }
